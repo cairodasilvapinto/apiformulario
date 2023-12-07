4b452a86c607cb91ada12d20c6db793cd9beb91b
cairodasilvapinto <cairosilva31@gmail.com>
2023-12-07 11:06:38 -0300

    teste

---
+++ FRONT/UIforms/src/components/Buy.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Buy = () => {
+  return (
+    <div>
+      <h1>Buy Page</h1>
+      {/* Add your buying form or content here */}
+    </div>
+  );
+};
+
+export default Buy;
